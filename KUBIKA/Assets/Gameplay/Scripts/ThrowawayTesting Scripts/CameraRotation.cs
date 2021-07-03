fec5be75a7c3ff4d575b9e2ef917be2ae3df6cfe
Léonard <epoc4oftime@gmail.com>
2021-07-04 01:29:13 +0200

    Game Camera tests

--- KUBIKA/Assets/Gameplay/Scripts/ThrowawayTesting Scripts/CameraRotation.cs
+++ KUBIKA/Assets/Gameplay/Scripts/ThrowawayTesting Scripts/CameraRotation.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
-    private Vector3 rotation;
+    public Camera sceneCamera;
+    private Vector3 baseRotation;
+    public Transform target;
 
-    private void Awake()
+    private void Start()
     {
-        rotation = transform.eulerAngles;
+        baseRotation = sceneCamera.transform.eulerAngles;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        rotation.y += Time.deltaTime * 25f;
-        transform.eulerAngles = rotation;
+        if (sceneCamera.transform.eulerAngles.y < baseRotation.y + 60)
+        {
+            if(Input.GetKey(KeyCode.LeftArrow)) transform.RotateAround(new Vector3(3.60000014f, 0f, 3.60000014f) / 2, Vector3.up, 90 * Time.deltaTime);
+        }
+
+        if (sceneCamera.transform.eulerAngles.y > baseRotation.y - 60)
+        {
+            if(Input.GetKey(KeyCode.RightArrow)) transform.RotateAround(new Vector3(3.60000014f, 0f, 3.60000014f) / 2, Vector3.up, -90 * Time.deltaTime);
+        }
     }
 }
